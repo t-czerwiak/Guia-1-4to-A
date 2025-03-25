0c55072fef7ed429e210a4543ea52fd94bfecaef
T-CZERWIAK <157724722+t-czerwiak@users.noreply.github.com>
2025-03-25 12:11:48 -0300

    Ejercicio 1.2 terminado

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string nombreUsuario;
+    [SerializeField] int edadUsuario;
+    public float estaturaUsuario;
+    [SerializeField] bool donanteDeOrganos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es "+ nombreUsuario + ", tengo "+ edadUsuario + ", mido "+ estaturaUsuario +" metros y es "+ donanteDeOrganos +" que soy donante de organos");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
