d09985df528c462be3a705309c99fa7157948c5e
T-CZERWIAK <157724722+t-czerwiak@users.noreply.github.com>
2025-04-01 12:00:50 -0300

    espacios agregados

--- Assets/Scripts/CalculosPersonalizados.cs
+++ Assets/Scripts/CalculosPersonalizados.cs
@@ -11,12 +11,16 @@ public class CalculosPersonalizados : MonoBehaviour
     {
         resultado = num1 + num2;
         Debug.Log("El resultado de la suma entre " + num1 + " y " + num2 + " es: " + resultado);
+
         resultado = num1 - num2;
         Debug.Log("El resultado de la resta entre " + num1 + " y " + num2 + " es: " + resultado);
+
         resultado = num1 * num2;
         Debug.Log("El resultado de la multiplicacion entre " + num1 + " y " + num2 + " es: " + resultado);
+
         resultado = num1 / num2;
         Debug.Log("El resultado de la division entre " + num1 + " y " + num2 + " es: " + resultado);
+
     }
 
     // Update is called once per frame
