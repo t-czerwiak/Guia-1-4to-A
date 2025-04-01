11715721a224193b130b18265d02e229edc14c88
T-CZERWIAK <157724722+t-czerwiak@users.noreply.github.com>
2025-04-01 12:10:55 -0300

    NumUno completado

---
+++ Assets/Scripts/NumUno.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumUno : MonoBehaviour
+{
+    int num1 = 0;
+    void Start()
+    {
+        num1 = num1 + 2; //num1 += 2;
+        Debug.Log(num1);
+        num1 = num1 * num1; //num1 *= num1;
+        Debug.Log(num1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
