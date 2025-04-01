6f6b8b97774fcf8493b3a379a0f82a436c872e6e
T-CZERWIAK <157724722+t-czerwiak@users.noreply.github.com>
2025-04-01 11:38:38 -0300

    empezamos ejercicio 1.3

--- Assets/Scripts/DatosDelUsuario.cs
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -12,7 +12,8 @@ public class DatosDelUsuario : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Mi nombre es "+ nombreUsuario + ", tengo "+ edadUsuario + ", mido "+ estaturaUsuario +" metros y es "+ donanteDeOrganos +" que soy donante de organos");
+        Debug.Log("Mi nombre es " + nombreUsuario + ", tengo " + edadUsuario + ", mido " + estaturaUsuario 
+            + " metros y es " + donanteDeOrganos + " que soy donante de organos");
     }
 
     // Update is called once per frame
