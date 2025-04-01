6643c8751aeec50f8f66b4da7d595144a4dc4a58
T-CZERWIAK <157724722+t-czerwiak@users.noreply.github.com>
2025-04-01 11:52:08 -0300

    1.3 ejercicio terminado

--- Assets/Scripts/Calculos.cs
+++ Assets/Scripts/Calculos.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Calculos : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int num1;
+    public int num2;
+    public int resultado;
+
     void Start()
     {
-        
+        resultado = num1 + num2;
+        Debug.Log(resultado);
     }
 
     // Update is called once per frame
