229a9896fe73cc133c786d9b686eb7b88543fc98
T-CZERWIAK <157724722+t-czerwiak@users.noreply.github.com>
2025-04-08 12:12:20 -0300

    que orientacion sos

---
+++ Assets/Scripts/DeQueOrientacionSos.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeQueOrientacionSos : MonoBehaviour
+{
+    public string nombreAlumno;
+    public int año;
+    public string orientacion;
+    void Start()
+    {
+        if (nombreAlumno == "")
+        {
+            Debug.Log("Error, el nombre no puede ser vacio");
+            return;
+        }
+
+        if (año != 1 & año != 2 & año != 3 & año != 4 & año != 5)
+        {
+            Debug.Log("Error, no es valido el año");
+            return;
+        }
+
+        else if (año < 3 & año >= 1)
+        {
+            Debug.Log("Error, seguis en siclo basico");
+            return;
+        }
+
+        if (orientacion != "T" & orientacion != "D" & orientacion != "G" & orientacion != "M" & orientacion != "H")
+        {
+            Debug.Log("Error, solo se puede ingresar T, D, G, M o H");
+            return;
+        }
+
+        else
+        {
+            Debug.Log("Muchas gracias " + nombreAlumno);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
