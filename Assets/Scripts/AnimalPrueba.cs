c6dd8063528f287cedd7928bfba3b73ab666fee7
T-CZERWIAK <157724722+t-czerwiak@users.noreply.github.com>
2025-04-08 11:43:23 -0300

    Conversion de Moneda terminado

---
+++ Assets/Scripts/AnimalPrueba.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalPrueba : MonoBehaviour
+{
+
+public string codigoAnimal;
+public int cantidadDias;
+int totalGramos;
+float costoAlimento;
+
+void Start()
+    {
+		if (cantidadDias <= 3)
+		{
+			Debug.Log("error, la cantidad de dias tiene que ser mayor que 3");
+			return;
+		}
+
+		if (codigoAnimal == "G")
+		{
+			totalGramos = cantidadDias * 300;
+			costoAlimento = totalGramos / 100 * 80;
+			Debug.Log("Para este periodo se necesitan " + totalGramos + "gramos de alimento. ");
+			Debug.Log("Costo de esa cantidad de alimento: $" + costoAlimento);
+		}
+
+		else if (codigoAnimal == "PP")
+		{
+			totalGramos = cantidadDias * 400;
+			costoAlimento = totalGramos / 100 * 80;
+			Debug.Log("Para este periodo se necesitan " + totalGramos + "gramos de alimento. ");
+			Debug.Log("Costo de esa cantidad de alimento: $" + costoAlimento);
+		}
+
+		else if (codigoAnimal == "PG")
+		{
+			totalGramos = cantidadDias * 700;
+			costoAlimento = totalGramos / 100 * 80;
+			Debug.Log("Para este periodo se necesitan " + totalGramos + "gramos de alimento. ");
+			Debug.Log("Costo de esa cantidad de alimento: $" + costoAlimento);
+		}
+
+		else if (codigoAnimal != "G" && codigoAnimal != "PP" && codigoAnimal != "PG")
+		{
+			Debug.Log("error, codigoAnimal solo puede valer G por gato, PP por perro pequeño, o PG por perro grande");
+			return;
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ConversionDeMoneda.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversionDeMoneda : MonoBehaviour
+{
+    public float montoAConvertir;
+    public string codigoMoneda;
+    float cotizacionMoneda;
+    float montoConvertido;
+    void Start()
+    {
+        if (montoAConvertir < 1000)
+        {
+            Debug.Log("El monto minimo es $1000");
+            return;
+        }
+
+        if (codigoMoneda == "D")
+        {
+            cotizacionMoneda = 1204.35f;
+        }
+
+        else if (codigoMoneda == "E")
+        {
+            cotizacionMoneda = 1541.11f;
+        }
+
+        else if (codigoMoneda == "R")
+        {
+            cotizacionMoneda = 201.24f;
+        }
+
+        else
+        {
+            Debug.Log("Opcion de moneda extranjera no valida");
+            return;
+        }
+
+        montoConvertido = montoAConvertir / cotizacionMoneda;
+
+        Debug.Log("Monto convertido: " + montoConvertido);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
